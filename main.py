d61555968bbb4db15a92f580706b48f89b3b9bcb
FM8299COM <karlis21kesans@gmail.com>
2020-10-19 13:16:13 +0300

    6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -25,6 +25,26 @@ def dontC():
 @app.route('/chattingP')
 def chattingP():
     return render_template('chattingP.html')
+
+@app.route("/PostMsg", methods=["POST"])
+def PostMsg():
+    return "This is post metode!"
+
+@app.route("/info", methods=["GET","POST"])
+def info():
+    if request.method == "GET":
+        vards = request.args.get("vards")
+        uzvards = request.args.get("uzvards")
+        return f"Esi sveiks {vards} {uzvards}"
+    elif request.method == "POST":
+        if request.content_type == "application/json":
+            dati = request.json
+            print(dati)
+            return f"This is POST method with type {request.content_type}"
+        else:
+            return f"This content type is not allowed {request.content_type}"
+    else:
+        return "Method unknown"
 
 app.run(host='0.0.0.0', port=80, debug=True)
 
